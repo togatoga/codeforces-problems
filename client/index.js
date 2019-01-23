51ace6a06ecdd6df245510279b485f5b2d51d17b
togatoga <togasakitogatoga@gmail.com>
2019-01-22 19:38:11 -0800

    Add App.js

--- client/index.js
+++ client/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {
+    Provider
+} from "react-redux";
+import {
+    createStore
+} from 'redux';
+import App from './components/App'
+
 
 ReactDOM.render(
-    <h1>Hello Codeforces problems!!</h1>,
+    <App />,
     document.getElementById("root")
 );
