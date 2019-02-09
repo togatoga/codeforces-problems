e0f5d48eaff736e7b88d25141a2cbbf75802449d
togatoga <togasakitogatoga@gmail.com>
2019-02-09 16:14:40 +0900

    Update

--- client/components/Problems.js
+++ client/components/Problems.js
@@ -1,10 +1,13 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Table from "react-bootstrap/Table";
+import { Container, Row, Badge } from "react-bootstrap";
 import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
 
 export default class Problems extends React.Component {
+  tagsFormatter(tags) {
+    return tags.map(tag => {
+      return <Badge>{tag}</Badge>;
+    });
+  }
   render() {
     const { problems } = this.props;
     console.log(problems);
@@ -16,37 +19,9 @@ export default class Problems extends React.Component {
               Problem
             </TableHeaderColumn>
             <TableHeaderColumn dataField="points">Points</TableHeaderColumn>
-            <TableHeaderColumn dataField="tags">Tags</TableHeaderColumn>
-            {/* <thead class="thead-default">
-              <tr>
-                <th>Problem</th>
-                <th>Points</th>
-                <th>Tags</th>
-              </tr>
-            </thead>
-            <tbody>
-              {problems.length > 0 &&
-                problems.map(problem => (
-                  <tr>
-                    <td>
-                      <a
-                        href={`http://codeforces.com/problemset/problem/${
-                          problem.contest_id
-                        }/${problem.index}`}
-                        target="_blank"
-                      >
-                        {problem.name}
-                      </a>
-                    </td>
-                    <td>{problem.points}</td>
-                    <td>
-                      {problem.tags.map(tag => (
-                        <span class="label label-primary">{tag}</span>
-                      ))}
-                    </td>
-                  </tr>
-                ))}
-            </tbody> */}
+            <TableHeaderColumn dataField="tags" dataFormat={this.tagsFormatter}>
+              Tags
+            </TableHeaderColumn>
           </BootstrapTable>
         </Row>
       </Container>
