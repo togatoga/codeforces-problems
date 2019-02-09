be1d073d7c2c4103a65ace97067f6ac305f485fd
togatoga <togasakitogatoga@gmail.com>
2019-02-09 15:45:18 +0900

    WIP work

--- client/components/Problems.js
+++ client/components/Problems.js
@@ -1,43 +1,55 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Table from "react-bootstrap/Table";
+import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
 
 export default class Problems extends React.Component {
   render() {
     const { problems } = this.props;
+    console.log(problems);
     return (
-      <div id="container">
-        <table class="table table-striped">
-          <thead class="thead-default">
-            <tr>
-              <th>Problem</th>
-              <th>Points</th>
-              <th>Tags</th>
-            </tr>
-          </thead>
-          <tbody>
-            {problems.length > 0 &&
-              problems.map(problem => (
-                <tr>
-                  <td>
-                    <a
-                      href={`http://codeforces.com/problemset/problem/${
-                        problem.contest_id
-                      }/${problem.index}`}
-                      target="_blank"
-                    >
-                      {problem.name}
-                    </a>
-                  </td>
-                  <td>{problem.points}</td>
-                  <td>
-                    {problem.tags.map(tag => (
-                      <span class="label label-primary">{tag}</span>
-                    ))}
-                  </td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-      </div>
+      <Container>
+        <Row>
+          <BootstrapTable data={problems} striped={true} hover={true}>
+            <TableHeaderColumn dataField="name" isKey={true}>
+              Problem
+            </TableHeaderColumn>
+            <TableHeaderColumn dataField="points">Points</TableHeaderColumn>
+            <TableHeaderColumn dataField="tags">Tags</TableHeaderColumn>
+            {/* <thead class="thead-default">
+              <tr>
+                <th>Problem</th>
+                <th>Points</th>
+                <th>Tags</th>
+              </tr>
+            </thead>
+            <tbody>
+              {problems.length > 0 &&
+                problems.map(problem => (
+                  <tr>
+                    <td>
+                      <a
+                        href={`http://codeforces.com/problemset/problem/${
+                          problem.contest_id
+                        }/${problem.index}`}
+                        target="_blank"
+                      >
+                        {problem.name}
+                      </a>
+                    </td>
+                    <td>{problem.points}</td>
+                    <td>
+                      {problem.tags.map(tag => (
+                        <span class="label label-primary">{tag}</span>
+                      ))}
+                    </td>
+                  </tr>
+                ))}
+            </tbody> */}
+          </BootstrapTable>
+        </Row>
+      </Container>
     );
   }
 }
--- client/containers/AsyncApp.js
+++ client/containers/AsyncApp.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { fetchProblems } from "../actions/actions";
 import Problems from "../components/Problems";
 import { connect } from "react-redux";
+import Container from "react-bootstrap/Container";
 
 class AsyncApp extends Component {
   constructor(props) {
@@ -13,7 +14,12 @@ class AsyncApp extends Component {
     dispatch(fetchProblems());
   }
   render() {
-    return <Problems problems={this.props.problems} />;
+    const { problems } = this.props;
+    return (
+      <Container>
+        {problems.length > 0 && <Problems problems={problems} />}
+      </Container>
+    );
   }
 }
 
