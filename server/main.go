d42f8c5a18c24de84d962f6dd7b7ff6d9d967069
togatoga <togasakitogatoga@gmail.com>
2019-03-11 21:46:01 +0900

    Add InternalIPFilter

--- server/main.go
+++ server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -23,6 +24,46 @@ type config struct {
 	Username string
 	Password string
 	Database string
+}
+
+func InternalIPFilter() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			requestIP := net.ParseIP(c.RealIP())
+			//only allow private ip
+			for _, cidr := range []string{
+				"127.0.0.0/8",    // IPv4 loopback
+				"10.0.0.0/8",     // RFC1918
+				"172.16.0.0/12",  // RFC1918
+				"192.168.0.0/16", // RFC1918
+				"::1/128",        // IPv6 loopback
+				"fe80::/10",      // IPv6 link-local
+				"fc00::/7",       // IPv6 unique local addr
+			} {
+				_, block, err := net.ParseCIDR(cidr)
+				if err != nil {
+					c.Echo().Logger.Error(err)
+					return echo.ErrInternalServerError
+				}
+				if block.Contains(requestIP) {
+					return next(c)
+				}
+			}
+			//Allow prod ip
+			ips, err := net.LookupIP("togatoga.net")
+			if err != nil {
+				c.Echo().Logger.Error(err)
+				return echo.ErrInternalServerError
+			}
+			for _, ip := range ips {
+				if ip.To4().String() == requestIP.String() {
+					return next(c)
+				}
+			}
+
+			return echo.ErrUnauthorized
+		}
+	}
 }
 
 func run() {
@@ -54,9 +95,9 @@ func run() {
 	e.GET("api/v1/submissions", db.Submissions)
 
 	//update data
-	e.PUT("api/v1/problems", db.UpdateProblems)
-	e.PUT("api/v1/submissions/:user", db.UpdateSubmissions)
-	e.PUT("api/v1/contests", db.UpdateContests)
+	e.PUT("api/v1/problems", db.UpdateProblems, InternalIPFilter())
+	e.PUT("api/v1/submissions/:user", db.UpdateSubmissions, InternalIPFilter())
+	e.PUT("api/v1/contests", db.UpdateContests, InternalIPFilter())
 
 	e.Logger.Fatal(e.Start(":" + port))
 }
